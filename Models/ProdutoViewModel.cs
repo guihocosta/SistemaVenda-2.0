12e7909433bfae7db305268149d042dc9f5db615
Guilherme Costa <guiihocosta@gmail.com>
2025-03-13 16:41:09 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ProdutoViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SistemaVenda.Models
+{
+    public class ProdutoViewModel
+    {
+        public int? Codigo { get; set; }
+
+        [Required(ErrorMessage="Informe a Descrição do Produto!")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Informe a Quantidade em Estoque do Produto!")]
+        public double Quantidade { get; set; }
+
+        [Required(ErrorMessage = "Informe o Valor Unitário do Produto!")]
+        [Range(0.1, Double.PositiveInfinity)]
+        public decimal? Valor { get; set; }
+
+        [Required(ErrorMessage = "Informe a Categoria Produto!")]
+        public int? CodigoCategoria { get; set; }
+
+        public IEnumerable<SelectListItem>? ListaCategorias { get; set; }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaVenda.DAL; 
+
+namespace SistemaVenda
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer("Server=.;Database=Estoque;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=true"));
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSession();
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (!env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+
+}
