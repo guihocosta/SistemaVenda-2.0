1943b1dba86dd027e1c83fb5fbd9ae0197c01d02
Guilherme Costa <guiihocosta@gmail.com>
2025-03-24 18:06:39 -0300

    Relatório de Venda Produtos

--- Controllers/RelatorioController.cs
+++ Controllers/RelatorioController.cs
@@ -1,13 +1,43 @@
 ﻿
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using SistemaVenda.DAL;
+using SistemaVenda.Models;
 
 namespace SistemaVenda.Controllers
 {
     public class RelatorioController : Controller
     {
+        protected ApplicationDbContext mContext;
+
+        public RelatorioController(ApplicationDbContext context)
+        {
+            mContext = context;
+        }
+
         public IActionResult Grafico()
         {
+            var lista = mContext.VendaProdutos
+                .GroupBy(x => x.CodigoProduto)
+                .Select(y => new GraficoViewModel
+                {
+                    CodigoProduto = y.First().CodigoProduto,
+                    Descricao = y.First().Produto.Descricao,
+                    TotalVendidos = y.Sum(z => z.Quantidade)
+                }).ToList();
+
+            string valores = string.Empty;
+            string labels = string.Empty;
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                valores += lista[i].TotalVendidos.ToString() + ",";
+                labels += "'" + lista[i].Descricao.ToString() + "',";
+            }
+
+            ViewBag.Valores = valores;
+            ViewBag.Labels = labels;
             return View();
         }
     }
